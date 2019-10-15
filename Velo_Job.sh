bc3a606f2404d6f0456423190f1ac19db8b04add
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2019-10-14 23:56:03 -0400

    Update Velo_Job.sh

--- Velo_Job.sh
+++ Velo_Job.sh
@@ -1,4 +1,7 @@
-#-- Velocito vs. kallisto + bustools + loompy (v3.0.6) : 
+#--- Velocito vs. kallisto + bustools + loompy (v3.0.6) :
+#--- 10-14-2019
+#--- 1- kallisto + bustools + loompy (v3.0.6) : runibng time ~ 1h (8 threads)
+#--- See. http://linnarssonlab.org/loompy/kallisto/index.html
 
 #!/bin/bash
 #PBS -l nodes=1:ppn=8,walltime=05:00:00
@@ -13,11 +16,13 @@ module load kallisto/0.46.0
 export LANG=en_US.UTF-8 LANGUAGE=en_US.en LC_ALL=en_US.UTF-8
 
 loompy fromfq --threads 8 JB18031.loom JB18031 \
-/projects/nehard/SingleCell/Softwares/cellranger/human_GRCh38_gencode.v31 metadata.tab \
+/projects/nehard/SingleCell/Softwares/cellranger/human_GRCh38_gencode.v31 \
+metadata.tab \ #tab "\t" delimited table: containing sample name (name), technology (10xv2, 10xv3) and targetnumcells
 /projects/nehard/SingleCell/LifeSpan/fastq/JB18031/JB18031_GT18-06631_SI-GA-F4_S7_L007_R1_001.fastq.gz \
 /projects/nehard/SingleCell/LifeSpan/fastq/JB18031/JB18031_GT18-06631_SI-GA-F4_S7_L007_R2_001.fastq.gz  \
 
-#--- Cell ranger (v3) + Velocito 
+#--- 2- Cell ranger (v3) + Velocito: runibng time ~ 10h
+#--  See. http://velocyto.org/velocyto.py/tutorial/cli.html#run10x-run-on-10x-chromium-samples
 
 #!/bin/bash
 #PBS -l nodes=1:ppn=8,walltime=10:00:00
