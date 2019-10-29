a53880319a9d3f0f990a4e06c747407b5ad1f794
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2019-10-29 10:59:35 -0400

    Update Combine_loomfiles.py

--- Combine_loomfiles.py
+++ Combine_loomfiles.py
@@ -2,5 +2,5 @@ import os
 import loompy
 
 root = "/projects/nehard/SingleCell/Jupyter/Velocity/"
-files = [os.path.join(root, x) for x in ["./loom_files/JB18025_HI13.loom", "./loom_files/JB18031_HY5.loom", "./loom_files/JB18085_HC10.loom", './loom_files/JB18036_HO4.loom']]
+files = [os.path.join(root, x) for x in ["./your_path/object1.loom", "./your_path/object2.loom", "/your_path/object3.loom"]]
 loompy.combine(files, "Comb.loom")
